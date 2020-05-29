12f095b3ce2108dcd4aead22c8167d02fdaa4ecf
Ethan Frater <ebfrater@yahoo.com>
2020-05-29 13:46:56 -0400

    fixed comment lines

--- facebookblast.py
+++ facebookblast.py
@@ -16,13 +16,15 @@ with mouse.Events() as events:
         print('Click your target')
         try:
             if event.button == mouse.Button.left:
-                while 1: ''' Loop is broken by default with pyautogui's escape, putting the mouse in the top left corner'''
+                while 1: 
+                    ''' Loop is broken by default with pyautogui\'s escape, putting the mouse in the top left corner'''
                     print('innit')
                     print(event.button)
                     pyautogui.press('enter')
                     pyautogui.write(blaster)    
                     time.sleep(.01)
-            elif event.button == mouse.Button.right:  '''listener can be broken by right clicking'''  
+            elif event.button == mouse.Button.right:  
+                '''listener can be broken by right clicking'''  
                 break 
             else:
                 print(type(event))
